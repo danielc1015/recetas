0f1ccd992ef5afab609f502e2695792de3b2f5f4
Daniel <danielc1015@gmail.com>
2019-11-25 12:56:01 -0300

    ListView

--- app/src/main/java/com/example/buscadorrecetas/MainActivity.java
+++ app/src/main/java/com/example/buscadorrecetas/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.buscadorrecetas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,18 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnBuscar;
     EditText etBuscador;
-    TextView tvIngredientes;
+    TextView tvNombre;
     TextView tvCalorias;
+    ListView lvIngredientes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.etBuscador = (EditText) findViewById(R.id.etBuscador);
-        this.btnBuscar = (Button) findViewById(R.id.btnBuscar);
-        this.tvIngredientes = (TextView) findViewById(R.id.tvIngredientes);
-        this.tvCalorias = (TextView) findViewById(R.id.tvCalorias);
+        this.etBuscador     = (EditText) findViewById(R.id.etBuscador);
+        this.btnBuscar      = (Button)   findViewById(R.id.btnBuscar);
+        this.lvIngredientes = (ListView) findViewById(R.id.lvIngredientes);
+        this.tvNombre       = (TextView) findViewById(R.id.tvNombre);
+        this.tvCalorias     = (TextView) findViewById(R.id.tvCalorias);
 
         this.btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void buscar(String keywords) {
-        String app_id = "57024004";
+        String app_id  = "57024004";
         String app_key = "ee5d2f5e0ffe4aa969345020d3d7b024";
         String baseUrl = "https://test-es.edamam.com/search?";
-        String url = baseUrl + "app_id=" + app_id + "&app_key=" + app_key + "&q=" + keywords;
+        String url     = baseUrl + "app_id=" + app_id + "&app_key=" + app_key + "&q=" + keywords;
 
         StringRequest solicitud = new StringRequest(
                 Request.Method.GET,
@@ -60,20 +66,23 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Tenemos respuesta desde el servidor
                         try {
-                            JSONObject respuestaJSON = new JSONObject(response);
-                            JSONArray hits = respuestaJSON.getJSONArray("hits");
-                            JSONObject primerResultado = hits.getJSONObject(0);
-                            JSONObject receta = primerResultado.getJSONObject("recipe");
+                            JSONObject respuestaJSON    = new JSONObject(response);
+                            JSONArray hits              = respuestaJSON.getJSONArray("hits");
+                            JSONObject primerResultado  = hits.getJSONObject(0);
+                            JSONObject receta           = primerResultado.getJSONObject("recipe");
                             JSONArray ingredientesArray = receta.getJSONArray("ingredientLines");
 
-                            String calorias = receta.getString("calories");
-                            String ingredientes = "";
+                            String calorias = "Calorias: " + receta.getString("calories");
+                            String nombre = receta.getString("label");
+                            String [] ingredientes = new String[ingredientesArray.length()];
 
                             for (int i=0; i<ingredientesArray.length(); i++) {
-                                ingredientes += "-" + ingredientesArray.getString(i) + "\n";
+                                ingredientes [i] = ingredientesArray.getString(i);
                             }
 
-                            tvIngredientes.setText(ingredientes);
+                            tvNombre.setText(nombre);
+                            ArrayAdapter <String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, ingredientes);
+                            lvIngredientes.setAdapter(adapter);
                             tvCalorias.setText(calorias);
 
 
